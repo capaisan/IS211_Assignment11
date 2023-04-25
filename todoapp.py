0ee6cb6f8ab71c85caf88791f3348b2dd35d4cca
Tyler DeCaro <77017295+capaisan@users.noreply.github.com>
2023-04-24 22:22:17 -0400

    Still working on it

--- todoapp.py
+++ todoapp.py
@@ -6,15 +6,21 @@ todo_list = ['Buy Milk', 'Pay Rent']
 
 @app.route('/')
 def show_todo_list():
-    return render_template('todo_list.html')
+    name = request.args.get("name", "world")
+    todo_item = request.args.get("todo_item")
+    return render_template('todo_list.html', name=name)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    todo_item = request.form['todo_item']
-    todo_list.append(todo_item)
-
-    return redirect('/')
+    todo_item = request.form.get("todo_item")
+    if not todo_item:
+        return "Invalid Entry"
+    render_template("success.html")
+    # todo_item = request.form['todo_item']
+    # todo_list.append(todo_item)
+    #
+    # return redirect('/')
 
 
 
