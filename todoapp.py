86b67fb4583e61153a12d9209c0688a2e19f6f11
Tyler DeCaro <capaisan@gmail.com>
2023-04-25 14:03:51 -0400

    Improved functionality.

--- todoapp.py
+++ todoapp.py
@@ -2,30 +2,35 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-todo_list = []
+todo_list = {"Name": [], "Email": []}
+name_list = []
 
 @app.route('/')
 def show_todo_list():
-    name = request.args.get("name", "world")
-    todo_item = request.args.get("todo_item")
-    return render_template('todo_list.html', name=name)
+
+    return render_template('todo_list.html')
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     todo_item = request.form.get("todo_item")
+    email      = request.form.get("email")
     if not todo_item:
         return "Invalid Entry"
 
     todo_item = request.form['todo_item']
-    todo_list.append(todo_item)
+    # email = request.form['email']
+    todo_list["Name"].append(todo_item)
+    # todo_list["Email"].append(email)
+
 
     return redirect("/success")
 
 
 @app.route('/success', methods=['GET'])
 def success():
-    return render_template("success.html", todo_list=todo_list)
+    return render_template("success.html",
+                           todo_list=todo_list)
 
 
 @app.route('/previous', methods=['GET'])
