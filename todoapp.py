22380f8be0ee15b7bbdb3e49ea783381dcbb4cd5
Tyler DeCaro <capaisan@gmail.com>
2023-04-27 16:30:40 -0400

    Added drop down menu.

--- todoapp.py
+++ todoapp.py
@@ -2,26 +2,26 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-todo_list = {"Name": [], "Email": []}
-name_list = []
+todo_list = {"Name": [], "Email": [], "Priority": []}
+
 
 @app.route('/')
 def show_todo_list():
-
     return render_template('todo_list.html')
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     todo_item = request.form.get("todo_item")
-    email      = request.form.get("email")
-    if not todo_item:
+    email = request.form.get("email")
+    priority = request.form.get("priority")
+
+    if not todo_item or not email or not priority:
         return "Invalid Entry"
 
-    todo_item = request.form['todo_item']
-    # email = request.form['email']
     todo_list["Name"].append(todo_item)
-    # todo_list["Email"].append(email)
+    todo_list["Email"].append(email)
+    todo_list["Priority"].append(priority)
 
 
     return redirect("/success")
